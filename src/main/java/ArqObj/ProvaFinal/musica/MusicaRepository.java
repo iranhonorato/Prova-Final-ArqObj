b3dfdf85a34c2850cb8f2ad8572aed20c157e3a9
iran <iranilsonhonorato88@gmail.com>
2025-11-11 17:40:25 -0300

    feat: Implementando repository e service para musica

--- src/main/java/ArqObj/ProvaFinal/musica/MusicaRepository.java
+++ src/main/java/ArqObj/ProvaFinal/musica/MusicaRepository.java
@@ -1,4 +1,10 @@
 package ArqObj.ProvaFinal.musica;
 
-public class MusicaRepository {
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MusicaRepository extends JpaRepository<Musica, Integer> {
+    public Musica findByTitulo(String titulo);
 }
--- src/main/java/ArqObj/ProvaFinal/musica/MusicaService.java
+++ src/main/java/ArqObj/ProvaFinal/musica/MusicaService.java
@@ -1,4 +1,44 @@
 package ArqObj.ProvaFinal.musica;
 
+
+import ArqObj.ProvaFinal.autenticacao.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
 public class MusicaService {
+
+    @Autowired
+    private MusicaRepository repository;
+
+
+    public List<Musica> listarMusicas() {
+        return repository.findAll();
+    }
+
+
+    public Musica cadastrarMusica(Musica novaMusica) {
+        if (novaMusica == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "É necessário inserir os dados da música");
+        }
+        if (novaMusica.getTitulo() == null || novaMusica.getTitulo().isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Observe se todos os campos estão preenchidos corretamente");
+        }
+        repository.save(novaMusica);
+        return novaMusica;
+    }
+
+
+
+    public void deletarMusica(String titulo) {
+        Musica existente =  repository.findByTitulo(titulo);
+        if (existente == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Música não encontrada");
+        }
+        repository.delete(existente);
+    }
 }
