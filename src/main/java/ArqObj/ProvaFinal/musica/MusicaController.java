eed37df090ac120af2664cb9cccde7070730f957
iran <iranilsonhonorato88@gmail.com>
2025-11-11 18:19:00 -0300

    feat: Controller da classe Musica completo

--- src/main/java/ArqObj/ProvaFinal/musica/MusicaController.java
+++ src/main/java/ArqObj/ProvaFinal/musica/MusicaController.java
@@ -1,13 +1,11 @@
 package ArqObj.ProvaFinal.musica;
 
+import ArqObj.ProvaFinal.autenticacao.Usuario;
 import ArqObj.ProvaFinal.autenticacao.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -36,17 +34,24 @@ public class MusicaController {
         }
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Musica> buscarPorId(@PathVariable Integer id) {
-        try {
-            Optional<Musica> opt = musicaRepository.findById(id);
-            if (opt.isPresent()) {
-                return ResponseEntity.ok(opt.get());
-            } else {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-            }
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-        }
+
+    @PostMapping
+    public ResponseEntity<Musica> cadastrarMusica(
+            @RequestBody Musica novaMusica,
+            @RequestHeader("Authorization") String token) {
+
+        Usuario usuario = usuarioService.validarToken(token);
+
+        Musica salvo = musicaService.cadastrarMusica(novaMusica);
+        return ResponseEntity.ok(salvo);
+    }
+
+
+    @DeleteMapping("/{titulo}")
+    public void deletarMusica(
+            @PathVariable String titulo,
+            @RequestHeader(name = "token") String token) {
+        Usuario usuario = usuarioService.validarToken(token);
+        musicaService.deletarMusica(titulo);
     }
 }
