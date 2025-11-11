4adc09caa05298a72070bf426cd540fcc218694c
iran <iranilsonhonorato88@gmail.com>
2025-11-11 18:15:40 -0300

    feat: Métodos Get implementado em ambas as classes

--- src/main/java/ArqObj/ProvaFinal/musica/MusicaController.java
+++ src/main/java/ArqObj/ProvaFinal/musica/MusicaController.java
@@ -1,4 +1,52 @@
 package ArqObj.ProvaFinal.musica;
 
+import ArqObj.ProvaFinal.autenticacao.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/musicas")
 public class MusicaController {
+
+    @Autowired
+    private MusicaService musicaService;
+
+    @Autowired
+    private MusicaRepository musicaRepository;
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+
+    @GetMapping
+    public ResponseEntity<List<Musica>> listarMusicas() {
+        try {
+            List<Musica> lista = musicaService.listarMusicas();
+            return ResponseEntity.ok(lista);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Musica> buscarPorId(@PathVariable Integer id) {
+        try {
+            Optional<Musica> opt = musicaRepository.findById(id);
+            if (opt.isPresent()) {
+                return ResponseEntity.ok(opt.get());
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
 }
--- src/main/java/ArqObj/ProvaFinal/playlist/PlaylistController.java
+++ src/main/java/ArqObj/ProvaFinal/playlist/PlaylistController.java
@@ -1,4 +1,53 @@
 package ArqObj.ProvaFinal.playlist;
 
+
+import ArqObj.ProvaFinal.autenticacao.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/playlists")
 public class PlaylistController {
+
+    @Autowired
+    private PlaylistService playlistService;
+
+    @Autowired
+    private PlaylistRepository playlistRepository;
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public ResponseEntity<List<Playlist>> listarPlaylists() {
+        try {
+            List<Playlist> lista = playlistService.listarPlaylists();
+            return ResponseEntity.ok(lista);
+        } catch (ResponseStatusException e) {
+            return ResponseEntity.status(e.getStatusCode()).body(null);
+        } catch (Exception e) {
+            // erro inesperado
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Playlist> buscarPorId(@PathVariable Integer id) {
+        try {
+            Playlist playlist = playlistService.findeById(id);
+            return ResponseEntity.ok(playlist);
+        } catch (ResponseStatusException e) {
+            return ResponseEntity.status(e.getStatusCode()).body(null);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
 }
